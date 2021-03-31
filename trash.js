944d17f66f4bdde0ba4969ce9253d093494c9a67
radityaM <78536660+radityaM@users.noreply.github.com>
2021-03-31 20:24:33 +0700

    Add files via upload

---
+++ trash.js
@@ -0,0 +1,26 @@
+class Trash{
+    constructor(x,y,radius){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+
+        this.x=x;
+        this.y=y;
+        this.radius = radius;
+        this.image = loadImage("paper.png");
+        this.body = Bodies.circle(this.x,this.y,(this.radius-20)/2,options);
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.radius, this.radius);
+        pop();
+      }
+}
